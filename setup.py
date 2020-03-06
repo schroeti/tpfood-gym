b53d3fdbce22b0d0d6d5f743d5f21924b89676e9
schroeti <47750592+schroeti@users.noreply.github.com>
2020-03-06 20:54:12 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(name='gym_tpfood',
       version='0.0.1',
-      install_requires=['gym', 
+      install_requires=['gym>=0.2.3', 
                        'numpy'],
        packages=find_packages(), # And any other dependencies foo needs
      ) 
