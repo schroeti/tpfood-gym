0612adec7e204a440b84c867d38bf88f44b41c2e
schroeti <47750592+schroeti@users.noreply.github.com>
2020-03-06 18:48:12 +0100

    Find other missing packages

--- setup.py
+++ setup.py
@@ -2,5 +2,7 @@ from setuptools import setup
 
 setup(name='gym_tpfood',
       version='0.0.1',
-      install_requires=['gym']  # And any other dependencies foo needs
-) 
+      install_requires=['gym', 
+                       'numpy'],
+       packages=find_packages(), # And any other dependencies foo needs
+     ) 
