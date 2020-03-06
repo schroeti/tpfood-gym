6c4d25f417f96df72cdc2f4c698b5f6b6671d250
schroeti <47750592+schroeti@users.noreply.github.com>
2020-03-06 16:49:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_delivery',
+setup(name='gym_tpfood',
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
 ) 
