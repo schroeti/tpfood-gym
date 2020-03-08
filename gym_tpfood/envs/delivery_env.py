04b438a298b9f54dfaf365e2dbbe469301de0c12
schroeti <47750592+schroeti@users.noreply.github.com>
2020-03-08 17:10:11 +0100

    Update delivery_env.py

--- gym_tpfood/envs/delivery_env.py
+++ gym_tpfood/envs/delivery_env.py
@@ -110,9 +110,9 @@ class Delivery(discrete.DiscreteEnv):
                             done = False
                             taxi_loc = (row, col)
 
-                            if action == 0 and self.desc[2*row+1, 1 + col] != b"x":
+                            if action == 0 and self.desc[2*row, 1 + col] != b"x":
                                 new_row = min(row + 1, max_row)
-                            elif action == 1 and self.desc[2*row, 1 + col] != b"x":
+                            elif action == 1 and self.desc[2*row-1, 1 + col] != b"x":
                                 new_row = max(row - 1, 0)
                             if action == 2 and self.desc[1 + row, 2 * col + 2] == b":":
                                 new_col = min(col + 1, max_col)
