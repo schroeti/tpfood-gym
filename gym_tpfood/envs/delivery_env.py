1cc8281422aadc44640fcca98fd31c32255ccb94
schroeti <47750592+schroeti@users.noreply.github.com>
2020-09-26 23:22:06 +0200

    Update delivery_env.py

--- gym_tpfood/envs/delivery_env.py
+++ gym_tpfood/envs/delivery_env.py
@@ -307,6 +307,12 @@ class Delivery(DiscreteEnv):
         elif pass_idx_1 == len(self.locs):
             out[2 * taxi_row + 1][2 * taxi_col + 1] = utils.colorize(
             out[2 * taxi_row + 1][2 * taxi_col + 1], 'cyan', highlight=True)
+            
+            pi_1, pj_1 = self.locs[pass_idx_1]
+            out[2 * pi_1 + 1][2 * pj_1 + 1] = utils.colorize(out[2 * pi_1 + 1][2 * pj_1 + 1], 'cyan', bold=True)
+            
+            pi_2, pj_2  = self.locs[pass_idx_2]
+            out[2 * pi_2 + 1][2 * pj_2 + 1] = utils.colorize(out[2 * pi_2 + 1][2 * pj_2 + 1], 'magenta', bold=True)
      
        
    
@@ -314,12 +320,24 @@ class Delivery(DiscreteEnv):
         elif pass_idx_2 == len(self.locs):
             out[2 * taxi_row + 1][2 * taxi_col + 1] = utils.colorize(
             out[2 * taxi_row + 1][2 * taxi_col + 1], 'magenta', highlight=True)
+            
+            pi_1, pj_1 = self.locs[pass_idx_1]
+            out[2 * pi_1 + 1][2 * pj_1 + 1] = utils.colorize(out[2 * pi_1 + 1][2 * pj_1 + 1], 'cyan', bold=True)
+            
+            pi_2, pj_2  = self.locs[pass_idx_2]
+            out[2 * pi_2 + 1][2 * pj_2 + 1] = utils.colorize(out[2 * pi_2 + 1][2 * pj_2 + 1], 'magenta', bold=True)
         
         
         #passenger 1 AND 2 in taxi
         elif pass_idx_1 == len(self.locs) and pass_idx_2 == len(self.locs):
             out[2 * taxi_row + 1][2 * taxi_col + 1] = utils.colorize(
             out[2 * taxi_row + 1][2 * taxi_col + 1], 'white', highlight=True)
+            
+            pi_1, pj_1 = self.locs[pass_idx_1]
+            out[2 * pi_1 + 1][2 * pj_1 + 1] = utils.colorize(out[2 * pi_1 + 1][2 * pj_1 + 1], 'cyan', bold=True)
+            
+            pi_2, pj_2  = self.locs[pass_idx_2]
+            out[2 * pi_2 + 1][2 * pj_2 + 1] = utils.colorize(out[2 * pi_2 + 1][2 * pj_2 + 1], 'magenta', bold=True)
         
         
         di_1, dj_1 = self.locs[dest_idx_1]
